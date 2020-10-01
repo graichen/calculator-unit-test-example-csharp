786f014afcc274fd8d2fadd53927f7948fd96838
Cathy Graichen <graichen@ge.com>
2020-09-30 23:35:00 -0400

    simple calculator with unit tests

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("5 + 12.3 = {0}", Calculator.sum(5, 12.3));
+            System.Console.WriteLine("63 - 13.5 = {0}", Calculator.minus(63, 13.5));
+            System.Console.WriteLine("13.2 / -2.35 = {0}", Calculator.divide(13.2, -2.35));
+            System.Console.WriteLine("-4.32 * 11.875 = {0}", Calculator.multiply(-4.32, 11.875));
+      
+            System.Console.WriteLine("Thank you");
+        }
+    }
+}
---
+++ CalculatorAppTests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void sumTest()
+        {
+            Assert.AreEqual(Calculator.sum(-3.2, 2.1), -1.1);
+        }
+
+        [TestMethod()]
+        public void minusTest()
+        {
+            Assert.AreEqual(Calculator.minus(4.25, 1.125), 3.125);
+        }
+
+        [TestMethod()]
+        public void divideTest()
+        {
+            Assert.AreEqual(Calculator.divide(12, 4), 3);
+        }
+
+        [TestMethod()]
+        public void divideByZeroTest()
+        {
+            Assert.AreEqual(Calculator.divide(12, 0), double.PositiveInfinity);
+        }
+
+        [TestMethod()]
+        public void multiplyTest()
+        {
+            Assert.AreEqual(Calculator.multiply(1.5, 12), 18);
+        }
+    }
+}
